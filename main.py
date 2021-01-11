0af64823b5082277dc2131f2abdaaec2682e8199
relarizky <relarizky19@gmail.com>
2021-01-11 22:57:37 +0700

    added sum function

--- main.py
+++ main.py
@@ -4,6 +4,32 @@
 # Copyright © Relarizky 2021
 
 
-if __name__ == "__main__":
+from sys import argv, exit
 
-    pass
+
+def sum(*values: tuple) -> int:
+    """
+    sum all given values
+    """
+
+    result = 0
+
+    for value in values:
+        if not value.isdigit():
+            print("input needs to be number")
+            exit(1)
+
+        result += int(value)
+
+    return result
+
+
+if __name__ == "__main__":
+    # executed when user run this file
+    user_input = argv[1:]
+
+    if user_input == []:
+        print("please provide the input")
+        exit(0)
+
+    print(f"the result is\t: {sum(*user_input)}")
