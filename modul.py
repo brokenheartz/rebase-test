9cc2a1ee1f060ee4b8d11dc272804deb06807513
relarizky <relarizky19@gmail.com>
2021-01-11 23:10:09 +0700

    created modul.py

---
+++ modul.py
@@ -0,0 +1,22 @@
+# Author    : Relarizky
+# Github    : https://github.com/relarizky
+# ----------------------------------------
+# Copyright © Relarizky 2021
+
+
+def sum(*number_list: tuple) -> int:
+    """
+    sum all the given number values
+    """
+
+    result = 0
+
+    for number in number_list:
+        if not number.isdigit():
+            # do not process non-number input
+            continue
+
+        result += int(number)
+
+    return result
+
