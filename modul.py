6c7070cde4c377c6df91fb0c9162457a5b4b4db3
relarizky <relarizky19@gmail.com>
2021-01-12 11:34:51 +0700

    added multiply function

--- modul.py
+++ modul.py
@@ -20,3 +20,19 @@ def sum(*number_list: tuple) -> int:
 
     return result
 
+
+def multiply(*number_list: tuple) -> int:
+    """
+    multiply all the given number values each other
+    """
+
+    result = 0
+
+    for number in number_list:
+        if not number.isdigit():
+            # do not process non-number input
+            continue
+        
+        result *= int(number)
+
+    return result
